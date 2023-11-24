49900c6535cd4b9a9cf9418889d75b5d5cdc716c
Faisal7666 <151022235+faisalmaulana76@users.noreply.github.com>
2023-11-24 20:16:12 +0700

    add event listener to btn again
    create function to handle btn again click reset random number, message and background color

--- main.js
+++ main.js
@@ -5,22 +5,34 @@ const checkElement = document.querySelector(".btn.check");
 const scoreElement = document.querySelector(".label-score .score");
 const highScoreElement = document.querySelector(".label-highscore .highscore");
 const messageElement = document.querySelector(".message");
+const btnAgainElement = document.querySelector(".btn.again");
 
 let randOmNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
-let highScore;
+let highScore = 0;
 
-checkElement.addEventListener("click", () => {
+checkElement.addEventListener("click", function () {
   const userGuess = parseInt(userGuessElement.value);
   if (userGuess === randOmNumber) {
     messageElement.textContent = "Correct! You win";
     highScore = score > highScore ? score : highScore;
     highScoreElement.textContent = highScore;
     document.querySelector("body").style.backgroundColor = "#60b347";
+    this.disabled = true;
     return;
   }
   score -= 1;
   scoreElement.textContent = score;
   messageElement.textContent =
     userGuess > randOmNumber ? "To High!" : "To Low!";
+});
+
+btnAgainElement.addEventListener("click", () => {
+  checkElement.disabled = false;
+  randOmNumber = Math.floor(Math.random() * 20) + 1;
+  score = 20;
+  scoreElement.textContent = score;
+  userGuessElement.value = "";
+  messageElement.textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "#222";
 });

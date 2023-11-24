72d0526508cd3cc87e5b655b54ab22c30ae463ee
Faisal7666 <151022235+faisalmaulana76@users.noreply.github.com>
2023-11-24 19:52:29 +0700

    add event listener to button check
    create logic to handle user guess number and give a clue for user, the number is to high or to low.

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello World</h1>
-`;
+const userGuessElement = document.querySelector(".guess");
+const checkElement = document.querySelector(".btn.check");
+const scoreElement = document.querySelector(".label-score .score");
+const highScoreElement = document.querySelector(".label-highscore .highscore");
+const messageElement = document.querySelector(".message");
+
+let randOmNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+let highScore;
+
+checkElement.addEventListener("click", () => {
+  const userGuess = parseInt(userGuessElement.value);
+  if (userGuess === randOmNumber) {
+    messageElement.textContent = "Correct! You win";
+    highScore = score > highScore ? score : highScore;
+    highScoreElement.textContent = highScore;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    return;
+  }
+  score -= 1;
+  scoreElement.textContent = score;
+  messageElement.textContent =
+    userGuess > randOmNumber ? "To High!" : "To Low!";
+});
